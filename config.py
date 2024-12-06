c1fcbf58992d56aa02bb1860432ec040563dbeb5
Bruno R. Oliveira <168135189+Bruno-Rogerio@users.noreply.github.com>
2024-12-06 09:03:19 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 from dotenv import load_dotenv
 import os
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use a porta do Render ou 5000 como padrão
+    app.run(host="0.0.0.0", port=port)
+
 load_dotenv()
 
 MONGO_URI = os.getenv('MONGO_URI')
